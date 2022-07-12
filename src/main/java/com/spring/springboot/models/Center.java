22f4742f4a0ad60a28af10e6434fcd1eccdb43e9
shounakbasu <divyaranubasu@gmail.com>
2022-07-12 22:05:12 +0530

    Donations (model+repo) and Center model created

---
+++ src/main/java/com/spring/springboot/models/Center.java
@@ -0,0 +1,29 @@
+package com.spring.springboot.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Center
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "city", nullable = false)
+    private String city;
+    
+
+}
---
+++ src/main/java/com/spring/springboot/repository/DonationsRepository.java
@@ -0,0 +1,5 @@
+
+@Repository
+public interface DonationsRepository {
+    
+}
