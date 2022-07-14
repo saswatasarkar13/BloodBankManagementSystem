4679b8a0ce2e1def05a8c530b4c8b2e85f4a6914
saswatasarkar13 <saswatasarkar13@gmail.com>
2022-07-14 17:13:39 +0530

    Updated

---
+++ src/main/java/com/spring/springboot/controllers/ProcureBloodController.java
@@ -0,0 +1,26 @@
+package com.spring.springboot.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.springboot.common.Constants;
+import com.spring.springboot.models.ProcureBlood;
+import com.spring.springboot.services.ProcureBloodService;
+
+@Controller
+public class ProcureBloodController {
+    @Autowired
+    private ProcureBloodService procureBloodService;
+
+    @RequestMapping(value = "/procure")
+    public String procureBloodHandler (Model model)
+    {
+        
+        ProcureBlood obj = new ProcureBlood();
+        model.addAttribute("blood_groups", Constants.BLOOD_GROUPS);
+        model.addAttribute("procure", obj);   
+        return "/procure/form";
+    }    
+}
