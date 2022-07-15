7f1ef8c99301c8c046a147f1f341a8af18b6782c
vishalshaw03 <vsrk243@gmail.com>
2022-07-16 00:45:10 +0530

    user register updated

--- src/main/resources/static/js/userRegister.js
+++ src/main/resources/static/js/userRegister.js
@@ -1,5 +1,10 @@
 const API_URL = "http://localhost:3000/api";
 
+const fileInput = document.getElementById("fileInput");
+const dpInput = document.getElementById("dpInput");
+const dp = document.getElementById("dp");
+const editIcon = document.getElementById("edit-icon");
+
 const uploadFile = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
@@ -19,10 +24,20 @@ const uploadFile = async (file) => {
   return null;
 };
 
-const onChangeHandler = (event) => {
-  console.log(event);
+const onClickHandler = () => {
+  fileInput.click();
 };
 
-const fileInput = document.getElementById("fileInput");
+const onChangeHandler = async (event) => {
+  const f = event.target.files[0];
+  const res = await uploadFile(f);
 
-fileInput.addEventListener("change", onChangeHandler);
+  if (res) {
+    dpInput.value = res;
+    dp.src = res;
+  }
+};
+
+editIcon.addEventListener("click", onClickHandler);
+
+fileInput.addEventListener("change", onChangeHandler, false);
