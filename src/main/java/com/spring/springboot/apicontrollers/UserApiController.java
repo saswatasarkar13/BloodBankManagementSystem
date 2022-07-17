992379f69072051a0b444181ffbec463dbea42f4
Saswata Sarkar <saswatasarkar13@gmail.com>
2022-07-17 17:47:42 +0530

    added userApiController

---
+++ src/main/java/com/spring/springboot/apicontrollers/UserApiController.java
@@ -0,0 +1,47 @@
+package com.spring.springboot.apicontrollers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.springboot.models.User;
+import com.spring.springboot.services.UserService;
+
+@RestController
+@RequestMapping(value = "/api/user")
+public class UserApiController {
+
+    private UserService userService;
+
+    @RequestMapping (value = "/dp", method = RequestMethod.PUT)
+    public Map<String, Object> updateProfilePicture(@RequestBody Map<String, String> payload){
+    HashMap <String,Object> map = new HashMap<>();
+    try {
+        String uId = payload.get("id");
+        String udpLink = payload.get("dp");
+        User user = this.userService.findById(Long.parseLong(uId));
+        user.setDp(udpLink);
+
+        User userDp = this.userService.save(user);
+        if (userDp == null)
+        {
+            map.put("success",false);
+            return map;
+        }
+        map.put("success",true);
+        return map;
+        
+        
+    } catch (Exception e) 
+    {
+        e.printStackTrace();
+        map.put("success",false);
+            return map;
+    }
+
+    }
+}
