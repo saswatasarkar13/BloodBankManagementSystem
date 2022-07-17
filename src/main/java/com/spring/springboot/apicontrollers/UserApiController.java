94c1d15150dfdb24f0e9e71e4b233a2343bf7f34
Saswata Sarkar <saswatasarkar13@gmail.com>
2022-07-17 18:42:24 +0530

    added isActivelyDonating

--- src/main/java/com/spring/springboot/apicontrollers/UserApiController.java
+++ src/main/java/com/spring/springboot/apicontrollers/UserApiController.java
@@ -9,9 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.spring.springboot.models.Donation;
 import com.spring.springboot.models.User;
-import com.spring.springboot.services.DonationService;
 import com.spring.springboot.services.UserService;
 
 @RestController
