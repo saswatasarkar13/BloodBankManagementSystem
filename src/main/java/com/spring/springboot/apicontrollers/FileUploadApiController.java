e1a6e2cac825584506333f213dadf0a501876972
vishalshaw03 <vsrk243@gmail.com>
2022-07-18 13:15:15 +0530

    file upload removed

--- src/main/java/com/spring/springboot/apicontrollers/FileUploadApiController.java
+++ src/main/java/com/spring/springboot/apicontrollers/FileUploadApiController.java
@@ -3,43 +3,44 @@ package com.spring.springboot.apicontrollers;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.spring.springboot.helpers.FileUpload;
+// import com.spring.springboot.helpers.FileUpload;
 
 @RestController
 @RequestMapping(value = "/api/file")
 public class FileUploadApiController {
 
-    @Autowired
-    private FileUpload fileUploadHelper;
+    // @Autowired
+    // private FileUpload fileUploadHelper;
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
     public Map<String, Object> addProfilePicture(@RequestParam("file") MultipartFile file) {
         HashMap<String, Object> map = new HashMap<>();
-        try {
-            if (file.isEmpty()) {
-                map.put("success", false);
-                return map;
-            }
+        // try {
+        // if (file.isEmpty()) {
+        // map.put("success", false);
+        // return map;
+        // }
 
-            String filename = file.getOriginalFilename();
-            String path = "/uploads/" + filename;
+        // String filename = file.getOriginalFilename();
+        // String path = "/uploads/" + filename;
 
-            boolean res = fileUploadHelper.uploadFile(file);
+        // boolean res = fileUploadHelper.uploadFile(file);
 
-            map.put("filepath", path);
-            map.put("success", res);
-        } catch (Exception e) {
-            e.printStackTrace();
-            map.put("success", false);
-        }
+        // map.put("filepath", path);
+        // map.put("success", res);
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // map.put("success", false);
+        // }
 
+        map.put("success", false);
         return map;
     }
 }
--- src/main/java/com/spring/springboot/helpers/FileUpload.java
+++ src/main/java/com/spring/springboot/helpers/FileUpload.java
@@ -1,35 +1,35 @@
-package com.spring.springboot.helpers;
+// package com.spring.springboot.helpers;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+// import java.io.File;
+// import java.io.IOException;
+// import java.nio.file.Files;
+// import java.nio.file.Paths;
+// import java.nio.file.StandardCopyOption;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
+// import org.springframework.core.io.ClassPathResource;
+// import org.springframework.stereotype.Component;
+// import org.springframework.web.multipart.MultipartFile;
 
-@Component
-public class FileUpload {
-    public final String UPLOAD_DIR = new ClassPathResource("static/uploads/").getFile().getAbsolutePath();;
+// @Component
+// public class FileUpload {
+//     public final String UPLOAD_DIR = new ClassPathResource("static/uploads/").getFile().getAbsolutePath();;
 
-    public FileUpload() throws IOException {
+//     public FileUpload() throws IOException {
 
-    }
+//     }
 
-    public boolean uploadFile(MultipartFile file) {
+//     public boolean uploadFile(MultipartFile file) {
 
-        try {
-            String filePath = UPLOAD_DIR + File.separator + file.getOriginalFilename();
+//         try {
+//             String filePath = UPLOAD_DIR + File.separator + file.getOriginalFilename();
 
-            Files.copy(file.getInputStream(), Paths.get(filePath), StandardCopyOption.REPLACE_EXISTING);
+//             Files.copy(file.getInputStream(), Paths.get(filePath), StandardCopyOption.REPLACE_EXISTING);
 
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
+//             return true;
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//             return false;
+//         }
 
-    }
-}
+//     }
+// }
